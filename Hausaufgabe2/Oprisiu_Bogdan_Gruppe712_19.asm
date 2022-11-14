409338295fe9ba0cc2edd8a4790ee31d66e4dd56
Bogdan <bobo.oprisiu@gmail.com>
2022-11-14 10:45:33 -0800

    finish H2

--- Hausaufgabe2/Oprisiu_Bogdan_Gruppe712_19.asm
+++ Hausaufgabe2/Oprisiu_Bogdan_Gruppe712_19.asm
@@ -10,14 +10,14 @@ import exit msvcrt.dll    ; exit is a function that ends the calling process. It
 
 ; our data is declared here (the variables needed by our program)
 segment data use32 class=data
-    ;r dd 100100010000010000100 000000 01000b
+    ;r dd 10010001000001000010000000001000b
     ;t dd 01000010000000001000000100001001b
-    r dd 1001_0001_0000_0100_0010_0000_0000_1000b
-    t dd 0100_0010_0000_0000_1000_0001_0000_1001b
+    ;r^t= 11010011000001001010000100000001
+    r dd 10010001000001000010000000001000b
+    t dd 01000010000000001000000100001001b
     q dd 00000000000000000000000000000000b
-    ;q = 0000000 100000100101000010 0100000
-    ;    0100000 100000000001000010 0100000
-    ;r^t=1101001 100000100101000010 0000001
+    ;q = 00000001000001001010000100100000
+    ;q = 0104A120h
 
 ; our code starts here
 segment code use32 class=code
@@ -28,7 +28,8 @@ segment code use32 class=code
         mov edx, 0
         
         mov eax, [t]
-        and eax, 00000000000000011111110000000000b; eax enthalt die bits 10-16 von t
+        and eax, 00000000000000011111110000000000b; wir isolieren die bits 10-16 von t
+        ; eax enthalt die bits 10-16 von t
         ;eax =   00000000000000001000000000000000
         shr eax, 10; eax enthalt auf die position 0-6 die bits 10-16 von t
         ;eax =   00000000000000000000000000100000
@@ -36,17 +37,18 @@ segment code use32 class=code
         mov ebx, [t]
         mov ecx, [r]
         xor ecx, ebx; ecx = r xor t = 1101001100000100100000100000001
-        and ecx, 00000000_111111111111111111_000000; ecx = 000000_ die bits 7 - 24 von r xor t_000000 = 0000000 100000100101000010 0000000
+        and ecx, 00000001111111111111111110000000b; wir isolieren die bits 7-24 von r^t
+        ; ecx = 000000_ die bits 7 - 24 von r xor t_000000 = 00000001000001001010000100000000
         or eax, ecx; eax = 0000000_ die bits 7 - 24 von r xor t_ die bits 10-16 von t
 
         mov ecx, 0
         mov ebx, 0
         mov ecx, [r]
-        and ecx, 00000000000000000000111111100000b
+        and ecx, 00000000000000000000111111100000b; wir isolieren die bits 5-11 von r
         shl ecx, 20; die bits 5-11 von r _000000000000000000000000
         or eax, ecx; die bits 5-11 von r_ die bits 7 - 24 von r xor t_ die bits 10-16 von t
         
-        mov [q], eax 
+        mov [q], eax; = die bits 5-11 von r_ die bits 7 - 24 von r xor t_ die bits 10-16 von t
         
         ; exit(0)
         push    dword 0      ; push the parameter for exit onto the stack
--- Hausaufgabe2/Oprisiu_Bogdan_Gruppe712_2.asm
+++ Hausaufgabe2/Oprisiu_Bogdan_Gruppe712_2.asm
@@ -13,7 +13,8 @@ segment data use32 class=data
     a dw 0001000000001101b
     b dw 1100001100000001b
     c dd 00000000000000000000000000000000b
-    ;c = 0001000000001101 0000001000001001
+    ;c = 0001000000001101 0000001 000001 001b
+    ;c = 100D0209h
     
 ; our code starts here
 segment code use32 class=code
@@ -25,25 +26,28 @@ segment code use32 class=code
         mov edx, 0
         
         mov ax, [a]
-        and ax, 0111000000000000b ; ax enthalt die bits 12-14 von a
+        and ax, 0111000000000000b; wir isolieren die bits 12-14 von a: 001 
+        ; ax enthalt die bits 12-14 von a
         shr ax, 12; die bits 12-14 von ax stehen auf die positionen 0-2 im ax
         
         mov bx, [b]
-        and bx, 0000000000111111b; bx enthalt die ersten funf bits von
+        and bx, 0000000000111111b; wir isolieren die bits 0-5 von b: 000001
+        ; bx enthalt die ersten funf bits von
         shl bx, 3; bx enthalt auf die position 3-8 die bits 0-5 von b
         
-        or ax, bx; ax enthalt die bits 0-8 von c
+        or ax, bx; ax enthalt 0000000_ die bits 0-5 von b_die bits 12-14 von a
         
         mov bx, [a]
-        and bx, 0000001111111000b
+        and bx, 0000001111111000b; wir isoliearen die bits 3-9 von a : 0000001 
         shr bx, 3
-        shl bx, 9
+        shl bx, 9; wir tun die bits 3-9 von a auf die position 9-15
         
-        or ax, bx
+        or ax, bx; ax enthalt die bits 3-9 von a_ die bits_ die bits 0-5 von b_ die bits 12-14 von a
         
-        mov dx, [a]
-        shl edx, 16
+        mov dx, [a]; a = 0001000000001101 
+        shl edx, 16; wir bewegen die bits 0-15 von edx (die bits von a) auf die position 16-31
         or eax, edx; das resultat steht im eax
-        mov [c], eax
+        mov [c], eax ; c = a_ die bits 3-9 von a_ die bits_ die bits 0-5 von b_ die bits 12-14 von a
+        
         push    dword 0      ; push the parameter for exit onto the stack
         call    [exit]       ; call exit to terminate the program
